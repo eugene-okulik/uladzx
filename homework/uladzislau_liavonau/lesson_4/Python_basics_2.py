a53ffc7188d2306995aad284f4679d70e5cbc37b
Uladzislau Liavonau <uladzxliavonau@gmail.com>
2024-07-15 17:52:16 +0400

    Updated 'Python_basic_2' file: spaces after '#' were removed

--- homework/uladzislau_liavonau/lesson_4/Python_basics_2.py
+++ homework/uladzislau_liavonau/lesson_4/Python_basics_2.py
@@ -5,25 +5,25 @@ my_dict = {
     'set': {'set_1', 'set_2', 'set_3', 'set_4', 'set_5'}
 }
 
-#tuple
+# tuple
 
 print((my_dict['tuple'])[-1])
 
-#list
+# list
 
 add_new_element_in_list = my_dict['list'].append('list_6')
 remove_second_element_from_list = my_dict['list'].pop(1)
 
-#dict
+# dict
 
 add_new_element_in_dict = (my_dict['dict'])['i am a tuple'] = (10, 9, 8, 7, 6, 5)
 remove_any_element_from_dict = my_dict['dict'].pop('dict_1')
 
-#set
+# set
 
 add_new_element_in_set = my_dict['set'].add('set_new')
 remove_any_element_from_set = my_dict['set'].remove('set_3')
 
-#my_dict after all actions has the following view:
+# my_dict after all actions has the following view:
 
 print(my_dict)
