13b5f60d9612e0ccc2cdab3345d936e9d4186846
Uladzislau Liavonau <uladzxliavonau@gmail.com>
2024-07-17 13:42:25 +0400

    lesson_5_3 file fixed. (Duplicated empty string at the end of the lesson_5_3 file was removed)

--- homework/uladzislau_liavonau/lesson_5/lesson_5_3.py
+++ homework/uladzislau_liavonau/lesson_5/lesson_5_3.py
@@ -6,4 +6,3 @@ subjects = ['math', 'biology', 'geography']
 # result text should be: Students Ivanov, Petrov, Sidorov study these subjects: math, biology, geography
 
 print('Students', ', '.join(students) + ' study these subjects:', ', '.join(subjects))
-
