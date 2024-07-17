1ac140241b6dc9ff3fba65be0aeafcbb09fc2c4d
Uladzislau Liavonau <uladzxliavonau@gmail.com>
2024-07-17 13:34:45 +0400

    lesson_5_2 and lesson_5_3 files fixed

--- homework/uladzislau_liavonau/lesson_5/lesson_5_2.py
+++ homework/uladzislau_liavonau/lesson_5/lesson_5_2.py
@@ -5,19 +5,22 @@ result_2 = 'результат операции: 514'
 result_3 = 'результат работы программы: 9'
 
 # string #1
+# first_separator_index = result_1.index(':')
 
-first_number = int(result_1[(result_1.index('42')):]) + 10
-
-print(first_number)
+result_1_separated = result_1[(result_1.index(':')):]
+first_summ = int(result_1_separated[2:]) + 10
+print(first_summ)
 
 # string #2
+# second_separator_index = result_2.index(':')
 
-second_number = int(result_2[(result_2.index('514')):]) + 10
-
-print(second_number)
+result_2_separated = (result_2[(result_2.index(':')):])
+second_summ = int(result_2_separated[2:]) + 10
+print(second_summ)
 
 # string #3
+# third_separator_index = result_3.index(':')
 
-third_number = int(result_3[(result_3.index('9')):]) + 10
-
-print(third_number)
+result_3_separated = result_3[(result_3.index(':')):]
+third_summ = int(result_3_separated[2:]) + 10
+print(third_summ)
--- homework/uladzislau_liavonau/lesson_5/lesson_5_3.py
+++ homework/uladzislau_liavonau/lesson_5/lesson_5_3.py
@@ -5,7 +5,5 @@ subjects = ['math', 'biology', 'geography']
 
 # result text should be: Students Ivanov, Petrov, Sidorov study these subjects: math, biology, geography
 
-result = f'Students {students[0]}, {students[1]}, {students[2]} study these subjects: \
-{subjects[0]}, {subjects[1]}, {subjects[2]}'
+print('Students', ', '.join(students) + ' study these subjects:', ', '.join(subjects))
 
-print(result)
