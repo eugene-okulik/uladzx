09095a6b285cd2b28887e89f2d1e39f97820c35e
Uladzislau Liavonau <uladzxliavonau@gmail.com>
2024-12-09 12:17:50 +0400

    Lesson_21: Points mentioned in the previous review are fixed

--- test_api_uliavonau/conftest.py
+++ test_api_uliavonau/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import requests
+import allure
 
 from endpoints.create_object import CreateObject
 from endpoints.put_update_object import UpdateObjectPut
@@ -24,3 +26,20 @@ def patch_update_object_endpoint():
 @pytest.fixture()
 def delete_object_endpoint():
     return DeleteObject()
+
+
+@pytest.fixture()
+def create_and_delete_new_object():
+    body = {
+        "name": "10.10.2024 object",
+        "data": {"color": "red", "size": "large", "userId": 1}
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(
+        'http://167.172.172.115:52353/object',
+        json=body,
+        headers=headers
+    )
+    object_id = response.json()['id']
+    yield object_id
+    requests.delete(f'http://167.172.172.115:52353/object/{object_id}')
