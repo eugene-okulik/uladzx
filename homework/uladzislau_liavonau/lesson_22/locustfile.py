5e1a21ce66e13d2ac14e5b3ebdebe6c9cf1abd2c
Uladzislau Liavonau <uladzxliavonau@gmail.com>
2024-12-19 22:31:43 +0400

    Lesson_22: done (edited)

--- homework/uladzislau_liavonau/lesson_22/locustfile.py
+++ homework/uladzislau_liavonau/lesson_22/locustfile.py
@@ -1,1 +1,19 @@
 from locust import task, HttpUser
+import random
+
+
+class PerfTesting(HttpUser):
+
+    @task(1)
+    def get_all_objects(self):
+        self.client.get(
+            '/object',
+            headers={'Content-Type': 'application/json'}
+        )
+
+    @task(3)
+    def get_the_object(self):
+        self.client.get(
+            f'/object/{random.choice([1, 10, 1068, 1073])}',
+            headers={'Content-Type': 'application/json'}
+        )
