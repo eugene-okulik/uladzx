ea47ba667543696dbc31a8747307bd0c9ad48dd9
Uladzislau Liavonau <uladzxliavonau@gmail.com>
2025-08-01 16:58:40 +0400

    spaces between methods added

--- homework/uladzislau_liavonau/lesson_27/test_lesson_27.py
+++ homework/uladzislau_liavonau/lesson_27/test_lesson_27.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page, expect, BrowserContext
 
+
 # Task 1
 def test_check_the_alert(page: Page):
     page.goto('https://www.qa-practice.com/elements/alert/confirm')
@@ -7,6 +8,7 @@ def test_check_the_alert(page: Page):
     page.get_by_role('link', name='Click').click()
     result_message = page.locator("#result-text")
     expect(result_message).to_have_text("Ok")
+
 
 # Task 2
 def test_check_if_a_new_tab_appears(page: Page, context: BrowserContext):
@@ -19,6 +21,7 @@ def test_check_if_a_new_tab_appears(page: Page, context: BrowserContext):
     new_page = new_page_event.value
     result_message = new_page.locator('#result-text')
     expect(result_message).to_have_text("I am a new page in a new tab")
+
 
 # Task 3
 def test_wait_when_the_button_color_text_is_red(page: Page):
