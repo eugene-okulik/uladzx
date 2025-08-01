b25df85fa059d4f7abcacecd14de7cc92191590c
Uladzislau Liavonau <uladzxliavonau@gmail.com>
2025-08-01 16:57:27 +0400

    typo in the 'tab' word is fixed

--- homework/uladzislau_liavonau/lesson_27/test_lesson_27.py
+++ homework/uladzislau_liavonau/lesson_27/test_lesson_27.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page, expect, BrowserContext
 
-
+# Task 1
 def test_check_the_alert(page: Page):
     page.goto('https://www.qa-practice.com/elements/alert/confirm')
     page.on('dialog', lambda alert: alert.accept())
@@ -8,8 +8,8 @@ def test_check_the_alert(page: Page):
     result_message = page.locator("#result-text")
     expect(result_message).to_have_text("Ok")
 
-
-def test_check_if_a_new_tap_appears(page: Page, context: BrowserContext):
+# Task 2
+def test_check_if_a_new_tab_appears(page: Page, context: BrowserContext):
     page.goto("https://www.qa-practice.com/elements/new_tab/button")
     link = page.get_by_role('link', name='Click')
 
@@ -20,7 +20,7 @@ def test_check_if_a_new_tap_appears(page: Page, context: BrowserContext):
     result_message = new_page.locator('#result-text')
     expect(result_message).to_have_text("I am a new page in a new tab")
 
-
+# Task 3
 def test_wait_when_the_button_color_text_is_red(page: Page):
     page.goto('https://demoqa.com/dynamic-properties#google_vignette')
     color_button = page.locator('#colorChange')
